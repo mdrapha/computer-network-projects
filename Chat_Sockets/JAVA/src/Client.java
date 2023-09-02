265d855ee88a1890b00dcaf3932677c225b6a91f
Eduardo Verissimo <verissimo_eduardo@hotmail.com>
2023-09-01 21:42:54 -0300

    Server can handle multi clients

--- Chat_Sockets/JAVA/src/Client.java
+++ Chat_Sockets/JAVA/src/Client.java
@@ -4,35 +4,54 @@ import java.net.*;
 public class Client {
     public static void main(String[] args) {
         try {
-            Boolean running = true;
-
             Socket socket = new Socket("localhost", 49165); // Connect to server on localhost
             
             // Input and Output streams for communication
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader serverIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             
             System.out.println("Connected to server!\nWhat is your name? ");
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             String name = stdIn.readLine();
 
-            // Send a message to the server
+            // Send the user's name to the server
             out.println(name);
-            
 
-            while(running){
-
-                if(in.ready()){
-                    String serverResponse = in.readLine();
-                    System.out.println("Server response: " + serverResponse);
-                
+            // Create a thread to read messages from the server
+            Thread serverThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        String serverResponse = serverIn.readLine();
+                        if (serverResponse == null) {
+                            // Server has closed the connection
+                            break;
+                        }
+                        System.out.println(serverResponse);
+                    }
+                // Se a excessão for de socket close, apenas avisar que o chat foi encerrado, outras excessões devem ser mostradas o stack trace    
+                } 
+                catch (SocketException e) {
+                    System.out.println("Chat closed!");
                 }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            serverThread.start();
+
+            // Read and send user messages to the server
+            while (true) {
+                String userMessage = stdIn.readLine();
+                if (userMessage == null || userMessage.equalsIgnoreCase("/exit")) {
+                    // User wants to exit
+                    System.out.println("Closing connection...");
+                    break;
+                }
+                out.println(userMessage);
             }
-            // Close the streams and socket
-            in.close();
-            out.close();
+
+            // Close the socket
             socket.close();
- 
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Chat_Sockets/JAVA/src/Server.java
+++ Chat_Sockets/JAVA/src/Server.java
@@ -1,41 +1,104 @@
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
+    private static final int PORT = 49165;
+    // Map to store client usernames and their corresponding output streams
+    private static Map<String, PrintWriter> clientStreams = new HashMap<>();
+
     public static void main(String[] args) {
-     
         try {
-            ServerSocket serverSocket = new ServerSocket(49165); // Create a server socket on port 49165
-            System.out.println("Server started on port 49165");
-            Boolean running = true;
-            while (running) {
-                Socket socket = serverSocket.accept(); // Accept a connection from a client
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started on port " + PORT);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
                 System.out.println("Client connected: " + socket.getInetAddress().getHostAddress());
-                
+
                 // Input and Output streams for communication
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                
-                // Receive and print client's message
-                String clientMessage = in.readLine();
-                System.out.println("Client message: " + clientMessage);
-                
-                // Send a response message to the client
-                out.println("Hello " + clientMessage + "!");
-                
-                // Close the streams and socket
-                in.close();
-                out.close();
-                socket.close();
-            } 
-            serverSocket.close();
 
+                // Read the client's username
+                String username = in.readLine();
+                if (username != null) {
+                    synchronized (clientStreams) {
+                        clientStreams.put(username, out);
+                        for (Map.Entry<String, PrintWriter> entry : clientStreams.entrySet()) {
+                            if (!entry.getKey().equals(username)) {
+                                PrintWriter writer = entry.getValue();
+                                writer.println(username + " connected!");
+                            }
+                        }
+                    }
+                    
+                    // Create a new thread to handle client communication
+                    ClientHandler clientHandler = new ClientHandler(socket, in, username);
+                    Thread clientThread = new Thread(clientHandler);
+                    clientThread.start();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
-           
-        }
-        finally {
+        } finally {
             System.out.println("Server closed!");
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
+        private BufferedReader in;
+        private String username;
+
+        public ClientHandler(Socket socket, BufferedReader in, String username) {
+            this.socket = socket;
+            this.in = in;
+            this.username = username;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String clientMessage;
+                while ((clientMessage = in.readLine()) != null) {
+                    System.out.println(username + " said: " + clientMessage);
+
+                    // Send the received message to all clients except the sender
+                    synchronized (clientStreams) {
+                        for (Map.Entry<String, PrintWriter> entry : clientStreams.entrySet()) {
+                            if (!entry.getKey().equals(username)) {
+                                PrintWriter writer = entry.getValue();
+                                writer.println(username + " said: " + clientMessage);
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Remove the client's output stream from the map
+                synchronized (clientStreams) {
+                    clientStreams.remove(username);
+                }
+                
+                // Notify all clients that the user has disconnected
+                synchronized (clientStreams) {
+                    for (Map.Entry<String, PrintWriter> entry : clientStreams.entrySet()) {
+                        PrintWriter writer = entry.getValue();
+                        writer.println(username + " disconnected!");
+                    }
+                }
+
+                System.out.println("Client disconnected: " + username);
+            }
         }
     }
 }
